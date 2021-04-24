fb59e452d114ff7ab8329f88594f6645d31ca3ec
acannie <acannie0530@gmail.com>
2021-04-25 02:19:25 +0900

    code: resize picked image

--- lib/pick_image.dart
+++ lib/pick_image.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'dart:typed_data';
+import 'package:image/image.dart' as imageUtil;
+import 'dart:ui';
 
 // FEに画像がアップロードされたことを通知
 class PickedImageController with ChangeNotifier {
@@ -11,8 +14,10 @@ class PickedImageController with ChangeNotifier {
   Future<MemoryImage>? get imageFuture => _imageFuture;
 
   Future<MemoryImage> pickImage() async {
-    _imageFuture = ImagePicker().getImage(source: ImageSource.gallery).then(
-        (file) => file!.readAsBytes().then((bytes) => new MemoryImage(bytes)));
+    _imageFuture = ImagePicker()
+        .getImage(source: ImageSource.gallery)
+        .then((file) => file!.readAsBytes())
+        .then((bytes) => new MemoryImage(bytes));
 
     // For other components
     notifyListeners();
@@ -40,12 +45,22 @@ class PickedImageWidget extends StatelessWidget {
             if (snapshot.connectionState == ConnectionState.done &&
                 null != snapshot.data) {
               MemoryImage? image = snapshot.data;
+
               if (image != null) {
+                imageUtil.Image im = imageUtil.copyResize(
+                    imageUtil.decodeImage(image.bytes)!,
+                    width: 10,
+                    height: 10);
+
                 return InkWell(
                   onTap: () {
                     _future = controller.pickImage();
                   },
-                  child: Image.memory(image.bytes, fit: BoxFit.fill),
+                  child: Image.memory(
+                      Uint8List.fromList(imageUtil.encodeJpg(im)),
+                      width: 256,
+                      height: 256,
+                      fit: BoxFit.fill),
                 );
               }
             }
